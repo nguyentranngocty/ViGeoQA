1899698d912671fbb136306b51130a2411353e7f
TY NGUYEN TRAN NGOC <139307800+nguyentranngocty@users.noreply.github.com>
2025-12-28 13:13:47 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -67,7 +67,7 @@ class SeqReader(DatasetReader):
         self._target_token_indexer = target_token_indexer
         self._model_name = model_name
 
-        sub_dict_path = "data/sub_dataset_dict.pk"  # problems type
+        sub_dict_path = "ViGeoQA/sub_dataset_dict.pk"  # problems type
         with open(sub_dict_path, 'rb') as file:
             subset_dict = pickle.load(file)
         self.subset_dict = subset_dict
@@ -240,4 +240,5 @@ class SeqReader(DatasetReader):
         fields['point_label'] = ArrayField(np.array(point_label))
 
         return Instance(fields)
+
 
